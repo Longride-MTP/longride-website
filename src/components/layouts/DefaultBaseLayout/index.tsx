652e4efbb8b379c8a0728ab2ef2b2b7774451f0a
Wyzedix <wyzedix@gmail.com>
2025-04-01 12:23:06 +0200

    more prettier

--- src/components/layouts/DefaultBaseLayout/index.tsx
+++ src/components/layouts/DefaultBaseLayout/index.tsx
@@ -4,17 +4,17 @@ import Header from '../../sections/Header';
 import Footer from '../../sections/Footer';
 
 export default function DefaultBaseLayout(props) {
-    const { page, site } = props;
-    const { enableAnnotations = true } = site;
-    const pageMeta = page?.__metadata || {};
+	const { page, site } = props;
+	const { enableAnnotations = true } = site;
+	const pageMeta = page?.__metadata || {};
 
-    return (
-        <div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
-            <div className="sb-base sb-default-base-layout">
-                {site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
-                {props.children}
-                {site.footer && <Footer {...site.footer} enableAnnotations={enableAnnotations} />}
-            </div>
-        </div>
-    );
+	return (
+		<div className={classNames('sb-page', pageMeta.pageCssClasses)} {...(enableAnnotations && { 'data-sb-object-id': pageMeta.id })}>
+			<div className="sb-base sb-default-base-layout">
+				{site.header && <Header {...site.header} enableAnnotations={enableAnnotations} />}
+				{props.children}
+				{site.footer && <Footer {...site.footer} enableAnnotations={enableAnnotations} />}
+			</div>
+		</div>
+	);
 }
